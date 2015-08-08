043212ec7b5eed9f82853542980830a70524de94
Dave Bucholtz <dave@bucholtz.org>
2015-08-08 18:46:59 -0400

    Plot 4 final

--- plot4.R
+++ plot4.R
@@ -21,9 +21,25 @@ colnames(epc.data)    <- c('Date', 'Time', 'Global_active_power', 'Global_reacti
 ## Make the date and time a new column with date and time together
 epc.data              <- cbind(epc.data, observation.date = strptime(paste(epc.data[, "Date"], epc.data[, "Time"]), format = "%d/%m/%Y %H:%M:%S"))
 
-#Create Histogram of Global_active_power data
-hist(epc.data[, "Global_active_power"], xlab = "Global Active Power (kilowatts)", main = "Global Active Power", col = "red")
+par(mfrow = c(2, 2))
+
+#Create line chart of Global_active_power data vs. date
+plot(epc.data[, "observation.date"], epc.data[, "Global_active_power"], xlab = "", ylab = "Global Active Power", type = "l")
+
+#Create line chart of Voltage data vs. date
+plot(epc.data[, "observation.date"], epc.data[, "Voltage"], xlab = "datetime", ylab = "Voltage", type = "l")
+
+
+#Create line chart of Sub_metering_1, Sub_metering_2 and Sub_metering_3 data vs. date
+plot(epc.data[, "observation.date"], epc.data[, "Sub_metering_1"], type = "n", xlab = "", ylab = "Energy sub metering")
+lines(epc.data[, "observation.date"], epc.data[, "Sub_metering_1"], col = "black")
+lines(epc.data[, "observation.date"], epc.data[, "Sub_metering_2"], col = "red")
+lines(epc.data[, "observation.date"], epc.data[, "Sub_metering_3"], col = "blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1, 1, 1), col = c("black", "red", "blue"), bty = "n")
+
+#Create line chart of Global_reactive_power data vs. date
+plot(epc.data[, "observation.date"], epc.data[, "Global_reactive_power"], xlab = "datetime", ylab = "Global_reactive_power", type = "l")
 
 #write the file plot1.png
-dev.copy(device = png, "plot1.png", width = 480, height = 480)
+dev.copy(device = png, "plot4.png", width = 480, height = 480)
 dev.off()
